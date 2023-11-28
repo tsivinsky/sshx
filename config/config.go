a5240949d1172238a57bf3697932c35357811784
Joao Filipe Silva <joao.fcg.silva@gmail.com>
2023-11-28 14:13:25 +0000

    solved inadvert append when writing

--- config/config.go
+++ config/config.go
@@ -6,6 +6,12 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path"
+)
+
+var (
+	configDir  = "sshx"
+	configFile = "config.json"
 )
 
 type Prompter interface {
@@ -22,8 +28,7 @@ type Server struct {
 
 type Config struct {
 	Servers []Server `json:"servers"`
-	output  io.Writer
-	input   io.Reader
+	File    string
 }
 
 // used to override default behavior
@@ -31,9 +36,16 @@ type option func(*Config) error
 
 // used to initialize a new Config
 func NewConfig(opts ...option) (*Config, error) {
+	// sets user config dir
+	confDir, err := os.UserConfigDir()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+
+	// sets config.json filepath default when running as CLI
+	filepath := path.Join(confDir, configDir, configFile)
 	conf := &Config{
-		input:  os.Stdin,
-		output: os.Stdout}
+		File: filepath}
 	// loops through options to override defaults
 	for _, opt := range opts {
 		err := opt(conf)
@@ -45,30 +57,24 @@ func NewConfig(opts ...option) (*Config, error) {
 }
 
 // option to override input reading mode when used via CLI
-func WithFileInput(input io.Reader) option {
+func WithFile(file string) option {
 	return func(c *Config) error {
-		if input == nil {
-			return errors.New("nil input reader")
+		if file == "" {
+			return errors.New("nil file path")
 		}
-		c.input = input
-		return nil
-	}
-}
-
-// option to override output writing mode when used via CLI
-func WithFileOutput(output io.Writer) option {
-	return func(c *Config) error {
-		if output == nil {
-			return errors.New("nil output writer")
-		}
-		c.output = output
+		c.File = file
 		return nil
 	}
 }
 
 // load the configs from file or sdtIn
 func (conf *Config) Load() error {
-	data, err := io.ReadAll(conf.input)
+	file, err := os.OpenFile(conf.File, os.O_CREATE, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	data, err := io.ReadAll(file)
 	if err != nil {
 		return err
 	}
@@ -82,11 +88,20 @@ func (conf *Config) Load() error {
 
 // writes the configs to file or stdOut
 func (conf *Config) Write() error {
+	file, err := os.OpenFile(conf.File, os.O_RDWR, 0644)
+	if err != nil {
+		return err
+	}
+	// solves file appending, there might be a better way to go around this
+	// sshx add -> sshx remove to reproduce
+	os.Truncate(conf.File, 0)
+	defer file.Close()
 	data, err := json.MarshalIndent(conf, "", "  ")
 	if err != nil {
 		return err
 	}
+
 	// write to file
-	fmt.Fprintln(conf.output, string(data))
+	fmt.Fprint(file, string(data))
 	return nil
 }
--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"path"
 
 	ghPrompter "github.com/cli/go-gh/v2/pkg/prompter"
 	"github.com/tsivinsky/sshx/config"
@@ -12,48 +11,14 @@ import (
 
 var (
 	serverName = flag.String("name", "", "server name")
-	configDir  = "sshx"
-	configFile = "config.json"
 )
 
 func main() {
 	// parses cli flags
 	flag.Parse()
 
-	// sets user config dir
-	confDir, err := os.UserConfigDir()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-	}
-
-	// sets config.json filepath default when running as CLI
-	filepath := path.Join(confDir, configDir, configFile)
-
-	// opens $HOME/.config/sshx/config.json for reading
-	inFile, err := os.Open(filepath)
-	if err != nil {
-		// file doesn't exist, must be created
-		inFile, err := os.Create(filepath)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
-		}
-		defer inFile.Close()
-	}
-	defer inFile.Close()
-
-	// opens $HOME/.config/sshx/config.json for writing
-	outFile, err := os.OpenFile(filepath, os.O_RDWR, 0644)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-	}
-	defer outFile.Close()
-
 	// creates pointer config.Config using the constructure and overriding defaults with CLI values
-	conf, err := config.NewConfig(
-		config.WithFileInput(inFile),
-		config.WithFileOutput(outFile),
-	)
+	conf, err := config.NewConfig()
 	if err != nil {
 		fmt.Println("entre 1")
 		fmt.Fprintln(os.Stderr, err)

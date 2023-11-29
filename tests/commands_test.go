f582b617740374ab6cf0b8c45a932511c3e557f1
Joao Filipe Silva <joao.fcg.silva@gmail.com>
2023-11-29 10:19:54 +0000

    added TestRemove

--- tests/commands_test.go
+++ tests/commands_test.go
@@ -1,7 +1,6 @@
 package sshx_test
 
 import (
-	"os"
 	"reflect"
 	"testing"
 
@@ -11,8 +10,9 @@ import (
 type questionAnswer map[int]string
 
 type promptMock struct {
-	qa              questionAnswer
-	questionTracker int
+	qa                questionAnswer
+	questionTracker   int
+	MultiSelectAnswer []int
 }
 
 func (p *promptMock) Select(prompt string, defaultValue string, options []string) (int, error) {
@@ -25,22 +25,18 @@ func (p *promptMock) Input(prompt string, defaultValue string) (string, error) {
 }
 
 func (p *promptMock) MultiSelect(prompt string, defaultValues []string, options []string) ([]int, error) {
-	return []int{}, nil
+	return p.MultiSelectAnswer, nil
 }
 
 func TestAdd(t *testing.T) {
 	t.Parallel()
-	testFile, err := os.CreateTemp("", "*_test_config.json")
-
+	// test set up and tearDown
+	testFile, testConf, err := setUp()
+	defer tearDown(testFile)
 	if err != nil {
-		t.Errorf("error creating tempFile for testing purposes")
+		t.Errorf("error setting up test scenario: %v", err)
 	}
-	defer os.Remove(testFile.Name())
-
-	testConf, err := config.NewConfig(config.WithFile(testFile.Name()))
-	if err != nil {
-		t.Errorf("test add: error creating testConf")
-	}
+	// populate test data
 	testConf.Servers = []config.Server{{Name: "s1", User: "u1", Host: "h1"}}
 
 	pm := &promptMock{questionTracker: 0, qa: questionAnswer{1: "s2", 2: "u2", 3: "h2"}}
@@ -61,5 +57,30 @@ func TestAdd(t *testing.T) {
 }
 
 func TestRemove(t *testing.T) {
-
+	t.Parallel()
+	// test set up and tearDown
+	testFile, testConf, err := setUp()
+	defer tearDown(testFile)
+	if err != nil {
+		t.Errorf("error setting up test scenario: %v", err)
+	}
+	// populate test data
+	testConf.Servers = []config.Server{
+		{Name: "s1", User: "u1", Host: "h1"},
+		{Name: "s2", User: "u2", Host: "h2"},
+		{Name: "s3", User: "u3", Host: "h3"},
+		{Name: "s4", User: "u4", Host: "h4"}}
+	// populate mockPrompt with expected answers
+	// remove s2 and s4
+	mp := &promptMock{MultiSelectAnswer: []int{1, 3}}
+	err = testConf.Remove(mp)
+	if err != nil {
+		t.Errorf("TestRemove: error running TestConf.Remove(mp).\nerror: %v", err)
+	}
+	want := []config.Server{
+		{Name: "s1", User: "u1", Host: "h1"},
+		{Name: "s3", User: "u3", Host: "h3"}}
+	if got := testConf.Servers; !reflect.DeepEqual(got, want) {
+		t.Fatalf("got: %v, want %v", got, want)
+	}
 }
